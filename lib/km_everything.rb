2c31ff3a1bc6d3f728b0e92c86870ccc95838731
John Halloran & Trace Wax <pair+john+trace@tracedwax.com>
2013-04-10 20:48:02 -0400

    Allow users to specify events to exclude
    [Finishes #47850897]

--- lib/km_everything.rb
+++ lib/km_everything.rb
@@ -2,11 +2,14 @@ module KmEverything
   class << self
     attr_accessor :record_every_controller_action
     attr_accessor :event_names
+    attr_accessor :excluded_names
   end
 
   class KmEvent < Struct.new(:controller_name, :action_name)
     def event
-      specified_event || unspecified_event
+      unless event_excluded?
+        specified_event || unspecified_event
+      end
     end
 
     private
@@ -18,6 +21,18 @@ module KmEverything
 
     def unspecified_event
       "#{controller_name}##{action_name}" if KmEverything.record_every_controller_action
+    end
+
+    def event_excluded?
+      if excluded_names
+        if excluded_names[controller_name]
+          excluded_names[controller_name].include?(action_name)
+        end
+      end
+    end
+
+    def excluded_names
+      KmEverything.excluded_names
     end
   end
 end
--- spec/km_everything_spec.rb
+++ spec/km_everything_spec.rb
@@ -8,19 +8,77 @@ describe KmEverything::KmEvent do
       KmEverything.event_names = { "users" => { "show" => "Viewed User Page" } }
     end
 
-    context "when the controller name is present" do
-      let(:controller_name) { "users" }
+    describe "blacklisting events" do
+      context "when using a blacklist" do
+        before do
+          KmEverything.record_every_controller_action = true
+          KmEverything.excluded_names = { "notifications" => ["show", "index"]  }
+        end
 
-      context "when the action name is present" do
-        let(:action_name) { "show" }
+        context "and the event is on the blacklist" do
+          let(:controller_name) {"notifications"}
+          let(:action_name) { "show"}
 
-        it "returns the event name" do
-          km_event.event.should == "Viewed User Page"
+          it "does not return the event" do
+            km_event.event.should be_nil
+          end
+        end
+
+        context "and the event is not on the blacklist" do
+          let(:controller_name) {"sessions"}
+          let(:action_name) { "create"}
+
+          it "returns the event" do
+            km_event.event.should == "sessions#create"
+          end
+        end
+
+        after do
+          KmEverything.excluded_names = nil
+        end
+      end
+    end
+
+    describe "whitelisting events" do
+      context "when the controller name is present" do
+        let(:controller_name) { "users" }
+
+        context "when the action name is present" do
+          let(:action_name) { "show" }
+
+          it "returns the event name" do
+            km_event.event.should == "Viewed User Page"
+          end
+        end
+
+        context "when the action name is not present" do
+          let(:action_name) { "create" }
+
+          context "when the app is configured to record all controller actions" do
+            before do
+              KmEverything.record_every_controller_action = true
+            end
+
+            it "returns the controller and action name" do
+              km_event.event.should == "users#create"
+            end
+          end
+
+          context "when the app is configured to NOT record all controller actions" do
+            before do
+              KmEverything.record_every_controller_action = false
+            end
+
+            it "does not return an event name" do
+              km_event.event.should be_nil
+            end
+          end
         end
       end
 
-      context "when the action name is not present" do
-        let(:action_name) { "create" }
+      context "when the controller name is not present" do
+        let(:controller_name) { "some_other_controller" }
+        let(:action_name) { "some_other_action" }
 
         context "when the app is configured to record all controller actions" do
           before do
@@ -28,7 +86,7 @@ describe KmEverything::KmEvent do
           end
 
           it "returns the controller and action name" do
-            km_event.event.should == "users#create"
+            km_event.event.should == "some_other_controller#some_other_action"
           end
         end
 
@@ -40,31 +98,6 @@ describe KmEverything::KmEvent do
           it "does not return an event name" do
             km_event.event.should be_nil
           end
-        end
-      end
-    end
-
-    context "when the controller name is not present" do
-      let(:controller_name) { "some_other_controller" }
-      let(:action_name) { "some_other_action" }
-
-      context "when the app is configured to record all controller actions" do
-        before do
-          KmEverything.record_every_controller_action = true
-        end
-
-        it "returns the controller and action name" do
-          km_event.event.should == "some_other_controller#some_other_action"
-        end
-      end
-
-      context "when the app is configured to NOT record all controller actions" do
-        before do
-          KmEverything.record_every_controller_action = false
-        end
-
-        it "does not return an event name" do
-          km_event.event.should be_nil
         end
       end
     end
